653eea353c9840f26d4788008dde1ec7fde7bc5d
binyroo-book <binyroo@binyroo-book.local>
2013-06-20 19:31:29 +0900

    init 폴더 추가하도록 수정

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "init" --exclude ".osx" --exclude ".brew" \
+		--exclude ".osx" --exclude ".brew" \
 		--exclude "README.md" --exclude "LICENSE-GPL.txt" \
 		--exclude "LICENSE-MIT.txt" -av --no-perms . ~
 }
